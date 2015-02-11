564b0a7d4680180327da0e76d14262f002bdf8f5
Clark Zhang <blastpower2@gmail.com>
2015-02-10 19:10:39 -0500

    fixed bug in LaserCorrector

--- src/a1/SlamConstants.hpp
+++ src/a1/SlamConstants.hpp
@@ -3,13 +3,13 @@
 
 namespace eecs467 {
 
-static const int numParticles = 30;
+static const int numParticles = 1000;
 
 static const int emptyEvidenceStrength = 2;
 static const int occupiedEvidenceStrength = 1;
 
-static const float actionModelK1 = 0.05;
-static const float actionModelK2 = 0.05;
+static const float actionModelK1 = 0.2;
+static const float actionModelK2 = 0.2;
 
 }
 
