e37b8602b7d3273fbf8e096c6e6c7ce4cf66a9f9
Clark Zhang <blastpower2@gmail.com>
2015-02-16 12:20:02 -0500

    defined new header for Explore.hpp

--- src/a1/Explore.cpp
+++ src/a1/Explore.cpp
@@ -3,65 +3,20 @@
 #include "a1/SlamConstants.hpp"
 #include "a1/RobotConstants.hpp"
 
-eecs467::Explore::Explore() { }
+namespace eecs467 {
 
-void eecs467::Explore::furthestUnkownLoc(const maebot_processed_laser_scan_t& msg, const OccupancyGrid& grid){
+Explore::Explore() { }
 
-	int i = 0;
-	for ( ; i < msg.num_ranges; i++) {		
+Point<int> Explore::getNextWayPoint(const OccupancyGrid& grid, const Point<double>& currPos) {
 
-		Beam beam = { 
-			msg.ranges[i],
-			msg.thetas[i],
-			msg.x_pos[i],
-			msg.y_pos[i]
-		};
-	
-		beams.push_back(beam);
-	}
+}
 
-	// sort list of laser beams
-	std::sort(beams.begin(), beams.end(), eecs467::Explore::sortRanges);
-
-	Point<int> cellPos = { -1, -1 };
-
-	for(auto iter = beams.begin(); iter != beams.end(); iter++){
-		
-		//calculate endpoints
-		Point<float> point;
-		point.x = iter->posX + iter->range * wrap_to_pi(cos(iter->theta));
-		point.y = iter->posY + iter->range * wrap_to_pi(sin(iter->theta));
-
-		cellPos = global_position_to_grid_cell(point, grid);
-
-		if(grid.isCellInGrid(cellPos.x, cellPos.y)) {
-			if(grid(cellPos.x, cellPos.y) == 0) { //unexplored ????????????				
-				locs.push_back(cellPos); //add grid cell to deque
-			}
-		}		
-	
-	} 
-
-	//pop off furthest cell 
-	if(!locs.empty()) {
-		cellPos = locs.back();
-		locs.pop_back();
-		exploreX = cellPos.x;
-		exploreY = cellPos.y;
-	}
-	else {
-		exploreX = exploreX;
-		exploreY = exploreY;
-	}		 
-
-};
-
-eecs467::OccupancyGrid eecs467::Explore::getConfigurationSpace(const OccupancyGrid& grid) {
+OccupancyGrid Explore::getConfigurationSpace(const OccupancyGrid& grid, float radius) {
 	eecs467::OccupancyGrid configSpace(grid.widthInMeters(),
 		grid.heightInMeters(), grid.metersPerCell());
 
 	float cellDiagonal = sqrt(2) * grid.metersPerCell();
-	float deltaTheta = (cellDiagonal / 2) / (eecs467::baseLength / 2);
+	float deltaTheta = (cellDiagonal / 2) / radius;
 
 	for (int y = 0; y < (int)grid.heightInCells(); y++) {
 		for (int x = 0; x < (int)grid.widthInCells(); x++) {
@@ -86,14 +41,13 @@ eecs467::OccupancyGrid eecs467::Explore::getConfigurationSpace(const OccupancyGr
 	return configSpace;
 }
 
-void eecs467::Explore::drive(){ 
+std::vector<Point<int>> Explore::pickWayPoints(const OccupancyGrid& grid, const std::vector<Point<int>>& points) {
 
 }
 
-void eecs467::Explore::clearBeams() {
-	beams.clear();
+std::vector<Point<int>> Explore::breadthFirstSearch(const OccupancyGrid& grid, const Point<double>& currPos) {
+
 }
 
-void eecs467::Explore::clearLocs(){
-	locs.clear();
+
 }
